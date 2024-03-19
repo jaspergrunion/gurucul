f973010d789814d2b25c86b32c27cc4ee756366f
jgunninkaccenture <123487982+jgunninkaccenture@users.noreply.github.com>
2024-03-19 16:15:35 -0400

    Copying latest Gurucul model configs - 2024-03-19

--- model_config_tracker/gra_backup.sh
+++ model_config_tracker/gra_backup.sh
@@ -3,10 +3,10 @@
 # Create current date to use in file name
 current_date=$(date +"%Y-%m-%d")
 
-# Connect to MySQL
+# Connect to MySQL and pull model configs
 mysql -u root -p"mysql" -e "use gra; select * from analytical_models where enabled=1\G; quit;" > "./gra_models_$current_date.txt"
 
 git add -A
-git commit -m "Copying latest Gurucul model configs"
+git commit -m "Copying latest Gurucul model configs - $current_date"
 git push
 
