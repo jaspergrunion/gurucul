d915328b8c94418042751e537630d0f483d668fa
jgunninkaccenture <123487982+jgunninkaccenture@users.noreply.github.com>
2024-03-22 12:09:02 -0400

    added logic to conditionally commit files

--- model_config_tracker/gra_backup.sh
+++ model_config_tracker/gra_backup.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 # Script to pull model configuration files from Gurucul and back up to git
-target_dir=$"C:/Users/jgunn/gurucul/model_config_tracker/config_history"
+git_dir=$"C:/Users/jgunn/gurucul"
+output_dir=$"$git_dir/model_config_tracker/config_history"
+
+echo "Git directory: $git_dir"
+echo "Output directory: $output_dir"
+
+# Pull from git to make sure files are synced
+git -C $git_dir pull
 
 # Create current date to use in file name
 # current_date=$(date +"%Y-%m-%d")
 current_date=$(date +"%Y-%m-%d-%H-%M-%S")
 
 echo "Running model backup for $current_date"
-echo "Target directory: $target_dir"
 
 # Connect to MySQL and pull model configs
-mysql -u root -p"mysql" -e "use gra; select * from analytical_models where enabled=1\G;" > "$target_dir/gra_models_$current_date.txt"
+mysql -u root -p"mysql" -e "use gra; select * from analytical_models where enabled=1\G;" > "$output_dir/gra_models_$current_date.txt"
 
-latest_file=$(find $target_dir -type f -print | sort -r | head -1)
+latest_file=$(find $output_dir -type f -print | sort -r | head -1)
 echo "Latest file: $latest_file"
 
-compare_file=$(find $target_dir -type f -print | sort -r | head -2 | tail -1)
+compare_file=$(find $output_dir -type f -print | sort -r | head -2 | tail -1)
 echo "Compare file: $compare_file"
 
 # Commit files to git only if there is a difference
@@ -26,7 +32,7 @@ else
     echo "Files are different, commit to Git"
     echo "File differences:"
     diff $latest_file $compare_file
-    git add -A "$target_dir/*.txt"
-    git commit -m "Copying latest Gurucul model configs - $current_date"
-    git push
+    git -C $git_dir add -A "$output_dir/*.txt"
+    git -C $git_dir commit -m "Copying latest Gurucul model configs - $current_date"
+    git -C $git_dir push
 fi
