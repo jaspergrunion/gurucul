9614c080d680976ee8a6f809fb58a3d1752c5013
jgunninkaccenture <123487982+jgunninkaccenture@users.noreply.github.com>
2024-03-22 12:29:15 -0400

    added logic to conditionally commit files

--- model_config_tracker/gra.sql
+++ model_config_tracker/gra.sql
@@ -5,3 +5,4 @@ INSERT INTO analytical_models (id, anomaly_name, classifier_group, template_id, 
 INSERT INTO analytical_models (id, anomaly_name, classifier_group, template_id, category, enabled) VALUES (895, "Super Model 2", "ENTITY", 28, "Pattern Analysis", 1);
 INSERT INTO analytical_models (id, anomaly_name, classifier_group, template_id, category, enabled) VALUES (999, "Super Model 3", "ENTITY", 39, "Frequency Analysis", 1);
 INSERT INTO analytical_models (id, anomaly_name, classifier_group, template_id, category, enabled) VALUES (999, "Super Model 4", "ENTITY", 39, "Frequency Analysis", 1);
+INSERT INTO analytical_models (id, anomaly_name, classifier_group, template_id, category, enabled) VALUES (999, "Super Model 5", "ENTITY", 39, "Frequency Analysis", 1);
