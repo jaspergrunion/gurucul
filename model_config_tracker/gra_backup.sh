b425a7aa966cba056514a2658ee57e81f9fb7e36
jgunninkaccenture <123487982+jgunninkaccenture@users.noreply.github.com>
2024-03-22 11:52:19 -0400

    added logic to conditionally commit files

--- model_config_tracker/gra_backup.sh
+++ model_config_tracker/gra_backup.sh
@@ -5,7 +5,9 @@ target_dir=$"C:/Users/jgunn/gurucul/model_config_tracker/config_history"
 # Create current date to use in file name
 # current_date=$(date +"%Y-%m-%d")
 current_date=$(date +"%Y-%m-%d-%H-%M-%S")
-echo "Running for $current_date"
+
+echo "Running model backup for $current_date"
+echo "Target directory: $target_dir"
 
 # Connect to MySQL and pull model configs
 mysql -u root -p"mysql" -e "use gra; select * from analytical_models where enabled=1\G;" > "$target_dir/gra_models_$current_date.txt"
@@ -22,12 +24,9 @@ if cmp -s $latest_file $compare_file; then
     rm $latest_file
 else
     echo "Files are different, commit to Git"
+    echo "File differences"
     diff $latest_file $compare_file
     git add -A "$target_dir/*.txt"
     git commit -m "Copying latest Gurucul model configs - $current_date"
     git push
 fi
-
-# git add -A config_history/*.txt
-# git commit -m "Copying latest Gurucul model configs - $current_date"
-# git push
